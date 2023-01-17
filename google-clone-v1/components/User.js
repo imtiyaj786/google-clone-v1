7fb1323e08d74dfe6cc3cc48de4474eed9e6ad50
imtiyaj786 <imtiyaj1523@gmail.com>
2023-01-17 12:42:43 +0530

    add authentication and get user data from google

--- google-clone-v1/components/User.js
+++ google-clone-v1/components/User.js
@@ -1,3 +1,26 @@
-export default function User() {
-  return <h1>User</h1>;
+import { useSession, signIn, signOut } from "next-auth/react";
+export default function User({ className }) {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <img
+          onClick={signOut}
+          src={session.user.image}
+          alt="user-image"
+          className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1"
+        />
+      </>
+    );
+  }
+  return (
+    <>
+      <button
+        className="bg-blue-500 text-white px-6  py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+        onClick={signIn}
+      >
+        Sign in
+      </button>
+    </>
+  );
 }
