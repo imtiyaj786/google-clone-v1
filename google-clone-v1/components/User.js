754894366969af721cb251b91ff8f60b023e3cc9
imtiyaj786 <imtiyaj1523@gmail.com>
2023-01-17 12:48:17 +0530

    Add authentication and get user from google class changes

--- google-clone-v1/components/User.js
+++ google-clone-v1/components/User.js
@@ -8,7 +8,7 @@ export default function User({ className }) {
           onClick={signOut}
           src={session.user.image}
           alt="user-image"
-          className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1"
+          className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 ${className}`}
         />
       </>
     );
@@ -16,7 +16,7 @@ export default function User({ className }) {
   return (
     <>
       <button
-        className="bg-blue-500 text-white px-6  py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+        className={`bg-blue-500 text-white px-6 py-2  font-medium rounded-md hover:brightness-105 hover:shadow-md ${className}`}
         onClick={signIn}
       >
         Sign in
