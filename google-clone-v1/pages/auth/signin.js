1930d9edcca17417b34bb498bde07e1a2c1ade03
imtiyaj786 <imtiyaj1523@gmail.com>
2023-01-17 16:07:20 +0530

    Create body Section

--- google-clone-v1/pages/auth/signin.js
+++ google-clone-v1/pages/auth/signin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
+// import Image from "next/image";
 import { getProviders, signIn } from "next-auth/react";
 
 export default function signin({ providers }) {
--- google-clone-v1/pages/index.tsx
+++ google-clone-v1/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Image from "next/image";
+import { MagnifyingGlassIcon, MicrophoneIcon } from "@heroicons/react/24/solid";
+
 export default function Home() {
   return (
     <div>
@@ -12,6 +15,24 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* Body  */}
+      <form className="flex flex-col items-center mt-40">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+          alt=""
+          width="300"
+          objectFit="cover"
+          height="100"
+        />
+        <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+          <MagnifyingGlassIcon className="h-5 text-gray-500 mr-3" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm Feeling Lucky</button>
+        </div>
+      </form>
       {/* Footer */}
     </div>
   );
