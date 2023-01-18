01c6e546b06d9bbd2f61617f7c504a5c6c19ac4a
imtiyaj786 <imtiyaj1523@gmail.com>
2023-01-18 18:42:09 +0530

    Data from google api and create the mock data

--- google-clone-v1/pages/search.js
+++ google-clone-v1/pages/search.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
 
-export default function Search() {
+export default function Search(results) {
+  console.log("Hello", results);
   return (
     <div>
       <Head>
@@ -14,4 +15,22 @@ export default function Search() {
       {/* Search Results */}
     </div>
   );
+}
+export async function getServerSideProps(context) {
+  const startIndex = context.query.start || "1";
+  const mockData = false;
+  const data = mockData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }&start=${startIndex}`
+      ).then((response) => response.json());
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
