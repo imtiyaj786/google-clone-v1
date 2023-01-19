6688f8fbf97dcaf33086ac20fa6d3fd6dc52ba03
imtiyaj786 <imtiyaj1523@gmail.com>
2023-01-19 12:13:57 +0530

    Complete the result section and install html react parser

--- google-clone-v1/components/SearchResults.js
+++ google-clone-v1/components/SearchResults.js
@@ -1,13 +1,38 @@
 import React from "react";
+import Parser from "html-react-parser";
 export default function SearchResults({ results }) {
   console.log("SearchResult Components ", results);
   return (
     <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
       {/* <p>Hello Search Result Js</p> */}
+
+      {/* In this paragraph tag if I remove ? then I'm getting the error */}
+      {/* Unhandled Runtime Error
+            TypeError: Cannot read properties of undefined (reading 'formattedTotalResults') */}
       <p className="text-gray-600 text-sm mb-5 mt-3">
         About {results?.searchInformation?.formattedTotalResults} results (
         {results?.searchInformation?.formattedSearchTime} seconds)
       </p>
+      {/* Here problem */}
+
+      {results.items?.map((result) => (
+        <div key={result.link} className="max-w-xl mb-8">
+          <div className="group">
+            <a className="text-sm truncate" href={result.link}>
+              {result.formattedUrl}
+            </a>
+            <a
+              className="group-hover:underline decoration-blue-800"
+              href={result.link}
+            >
+              <h2 className="truncate text-xl font-medium text-blue-800">
+                {result.title}
+              </h2>
+            </a>
+          </div>
+          <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>
   );
 }
